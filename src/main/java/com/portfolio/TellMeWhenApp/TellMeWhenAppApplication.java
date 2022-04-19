eb240923d50f2181e271cf1c22d04c809c7fd84e
MaciexDrop <macwla1994@gmail.com>
2022-04-19 11:57:19 +0200

    Initial commit

---
+++ src/main/java/com/portfolio/TellMeWhenApp/TellMeWhenAppApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.TellMeWhenApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellMeWhenAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TellMeWhenAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/TellMeWhenApp/TellMeWhenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.TellMeWhenApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TellMeWhenAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
