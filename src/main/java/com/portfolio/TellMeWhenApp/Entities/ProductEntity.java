5ed27828b3d05ec758d8080be481b7ecd33bc5bf
MaciexDrop <macwla1994@gmail.com>
2022-04-21 12:03:29 +0200

    Add product entity class

--- src/main/java/com/portfolio/TellMeWhenApp/Entities/ProductEntity.java
+++ src/main/java/com/portfolio/TellMeWhenApp/Entities/ProductEntity.java
@@ -1,10 +1,14 @@
 package com.portfolio.TellMeWhenApp.Entities;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
 @Entity
 @Table(name = "products")
 public class ProductEntity {
@@ -15,4 +19,5 @@ public class ProductEntity {
     private String productName;
     private String productType;
     private String placeOfStorage;
+
 }
