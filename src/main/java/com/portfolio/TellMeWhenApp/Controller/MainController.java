82dff67ea135f141b450baeb6226f1e3ef7aa7dd
MaciexDrop <macwla1994@gmail.com>
2022-04-23 20:47:52 +0200

    Add logger to main controller for logging purposes :P

--- src/main/java/com/portfolio/TellMeWhenApp/Controller/MainController.java
+++ src/main/java/com/portfolio/TellMeWhenApp/Controller/MainController.java
@@ -1,7 +1,9 @@
 package com.portfolio.TellMeWhenApp.Controller;
 
-import com.portfolio.TellMeWhenApp.ModelDTO.ProductDTO;
+import com.portfolio.TellMeWhenApp.ModelDTO.ProductDto;
 import com.portfolio.TellMeWhenApp.Service.ProductServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Controller
 public class MainController {
+    public static final Logger LOGGER = LogManager.getLogger(MainController.class);
+
     ProductServiceImpl productService;
 
     public MainController(ProductServiceImpl productService) {
@@ -21,14 +25,15 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/addProduct")
-    public String newProduct(Model model, ProductDTO product) {
-        List<String> types = productService.getAllProductTypes();
-        List<String> places = productService.getAllProductPlacesOfStorage();
-        model.addAttribute("types", types);
-        model.addAttribute("places", places);
-        model.addAttribute("product", product);
-        return "addProduct";
+    @GetMapping("/addNewProduct")
+    public String newProduct(Model model, ProductDto productDto) {
+        List<String> productTypesList = productService.getAllProductTypes();
+        List<String> productPlacesOfStorageList = productService.getAllProductPlacesOfStorage();
+        model.addAttribute("types", productTypesList);
+        model.addAttribute("places", productPlacesOfStorageList);
+        model.addAttribute("product", productDto);
+        LOGGER.info("Added obiects to view model");
+        return "addProductForm";
     }
 
 }
