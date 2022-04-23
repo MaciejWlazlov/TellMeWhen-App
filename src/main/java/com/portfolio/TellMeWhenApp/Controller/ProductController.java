c6bd2261535838c5476a630e1b93bec2b7368fa3
MaciexDrop <macwla1994@gmail.com>
2022-04-23 20:52:39 +0200

    Refactor product controller - logger included

--- src/main/java/com/portfolio/TellMeWhenApp/Controller/ProductController.java
+++ src/main/java/com/portfolio/TellMeWhenApp/Controller/ProductController.java
@@ -1,29 +1,40 @@
 package com.portfolio.TellMeWhenApp.Controller;
 
-import com.portfolio.TellMeWhenApp.Model.ProductEntity;
-import com.portfolio.TellMeWhenApp.ModelDTO.ProductDTO;
+import com.portfolio.TellMeWhenApp.ModelDTO.ProductDto;
 import com.portfolio.TellMeWhenApp.Service.ProductServiceImpl;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @AllArgsConstructor
-@RestController
-@RequestMapping("/product")
+@Controller
 public class ProductController {
-    private ProductServiceImpl productService;
 
-    @PostMapping("/add")
-    public String addProduct(@ModelAttribute ProductDTO product, Model model) {
-        model.addAttribute("product", product);
-        productService.saveProduct(product);
-        return "result";
+    public static final Logger LOGGER = LogManager.getLogger(ProductController.class);
+    private final ProductServiceImpl productService;
+
+    @PostMapping("/addProduct")
+    public String addProduct(@ModelAttribute("productDto") @Valid ProductDto productDto, BindingResult result, Model model) {
+        if (result.hasFieldErrors()) {
+            List<String> productTypesList = productService.getAllProductTypes();
+            List<String> productPlacesOfStorageList = productService.getAllProductPlacesOfStorage();
+            model.addAttribute("types", productTypesList);
+            model.addAttribute("places", productPlacesOfStorageList);
+            LOGGER.warn("Errors in fields");
+            return "addProductForm";
+        } else {
+            productService.saveProduct(productDto);
+            LOGGER.info("Saved to the database successfully");
+        }
+        return "savedProduct";
     }
 
-    @GetMapping("/getAll")
-    public List<ProductEntity> getAllProducts() {
-        return productService.getAllProducts();
-    }
 }
