da2ab83dac6eede05992417587f7f0dbbcf7f995
MaciexDrop <macwla1994@gmail.com>
2022-04-23 20:49:04 +0200

    Add mapper for product dto -> entity conversion

---
+++ src/main/java/com/portfolio/TellMeWhenApp/Mapper/ProductDtoMapper.java
@@ -0,0 +1,22 @@
+package com.portfolio.TellMeWhenApp.Mapper;
+
+import com.portfolio.TellMeWhenApp.Model.ProductEntity;
+import com.portfolio.TellMeWhenApp.ModelDTO.ProductDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Data
+@Component
+public class ProductDtoMapper {
+
+    public ProductEntity mapProductIntoEntity(ProductDto productDto) {
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setProductName(productDto.name());
+        productEntity.setProductType(productDto.type());
+        productEntity.setPlaceOfStorage(productDto.place());
+        return productEntity;
+    }
+
+}
