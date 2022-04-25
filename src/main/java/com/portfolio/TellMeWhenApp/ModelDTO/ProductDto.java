fd9f7353fadf2748a1e05f251974fffc047a70fb
MaciexDrop <macwla1994@gmail.com>
2022-04-25 16:26:34 +0200

    Added validation annotations for DTO record

--- src/main/java/com/portfolio/TellMeWhenApp/ModelDTO/ProductDto.java
+++ src/main/java/com/portfolio/TellMeWhenApp/ModelDTO/ProductDto.java
@@ -1,14 +1,16 @@
 package com.portfolio.TellMeWhenApp.ModelDTO;
 
 import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public record ProductDto(
         @NotNull
-        @Length(min = 3,max = 20) String name,
-        @NotNull
+        @Length(min = 3, max = 20, message = "The product name should have at least 3 characters")
+        String name,
+        @NotBlank(message = "Please select product type")
         String type,
-        @NotNull
+        @NotBlank(message = "Please select a storage location for your product")
         String place) {
 }
 
