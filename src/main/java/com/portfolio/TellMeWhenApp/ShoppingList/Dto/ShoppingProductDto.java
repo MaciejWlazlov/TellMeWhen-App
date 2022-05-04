8c9d5f6b0684372534cb4f0a787bf63bac50373b
MaciexDrop <macwla1994@gmail.com>
2022-05-04 19:00:32 +0200

    Added shopping product DTO

---
+++ src/main/java/com/portfolio/TellMeWhenApp/ShoppingList/Dto/ShoppingProductDto.java
@@ -0,0 +1,15 @@
+package com.portfolio.TellMeWhenApp.ShoppingList.Dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public record ShoppingProductDto(
+        Integer id,
+        @NotNull
+        @Length(min = 3, max = 20, message = "The product name should have at least 3 characters")
+        String name,
+        @NotBlank(message = "Please select product type")
+        String type) {
+}
