0b0b635a0dd52c121c6dfd99a4a075cd07cbbe7b
MaciexDrop <macwla1994@gmail.com>
2022-05-04 19:06:29 +0200

    Refactored methods for storage product mappings

--- src/main/java/com/portfolio/TellMeWhenApp/Mapper/StorageProductMapper.java
+++ src/main/java/com/portfolio/TellMeWhenApp/Mapper/StorageProductMapper.java
@@ -1,9 +1,11 @@
 package com.portfolio.TellMeWhenApp.Mapper;
 
+import com.portfolio.TellMeWhenApp.StorageProduct.Dto.StorageProductDto;
 import com.portfolio.TellMeWhenApp.StorageProduct.Model.StorageProduct;
-import com.portfolio.TellMeWhenApp.StorageProductDto.StorageProductDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -14,29 +16,33 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class StorageProductMapper {
 
-    public StorageProduct mapProductIntoEntity(StorageProductDto storageProductDto) {
+    public static final Logger LOGGER = LogManager.getLogger(StorageProductMapper.class);
+
+    public StorageProduct mapDtoIntoEntity(StorageProductDto storageProductDto) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         StorageProduct storageProduct = new StorageProduct();
         storageProduct.setProductName(storageProductDto.name());
         storageProduct.setProductType(storageProductDto.type());
         storageProduct.setPlaceOfStorage(storageProductDto.place());
 
-        LocalDate purchaseDate = LocalDate.parse(storageProductDto.purchaseDate(), formatter);
-        storageProduct.setPurchaseDate(purchaseDate);
+        if (storageProductDto.purchaseDate().equals("")) {
+            storageProduct.setPurchaseDate(LocalDate.now());
+            LOGGER.warn("No purchase date selected -> inserting today's date");
+        } else {
+            LocalDate purchaseDate = LocalDate.parse(storageProductDto.purchaseDate(), formatter);
+            storageProduct.setPurchaseDate(purchaseDate);
+        }
         LocalDate expiryDate = LocalDate.parse(storageProductDto.expiryDate(), formatter);
         storageProduct.setExpiryDate(expiryDate);
-
         return storageProduct;
     }
 
-    public StorageProductDto mapProductIntoDto(StorageProduct storageProduct) {
-        return new StorageProductDto(
-                storageProduct.getId(),
+    public StorageProductDto mapEntityIntoDto(StorageProduct storageProduct) {
+        return new StorageProductDto(storageProduct.getId(),
                 storageProduct.getProductName(),
                 storageProduct.getProductType(),
                 storageProduct.getPlaceOfStorage(),
                 storageProduct.getPurchaseDate().toString(),
                 storageProduct.getExpiryDate().toString());
     }
-
 }
