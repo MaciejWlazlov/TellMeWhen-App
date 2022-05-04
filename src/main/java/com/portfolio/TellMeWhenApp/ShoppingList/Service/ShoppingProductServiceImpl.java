52decba449d4c65cdd9acb90176ab16b8b19bda1
MaciexDrop <macwla1994@gmail.com>
2022-05-04 19:05:10 +0200

    Added methods for shopping product mappings

--- src/main/java/com/portfolio/TellMeWhenApp/ShoppingList/Service/ShoppingProductServiceImpl.java
+++ src/main/java/com/portfolio/TellMeWhenApp/ShoppingList/Service/ShoppingProductServiceImpl.java
@@ -1,13 +1,49 @@
 package com.portfolio.TellMeWhenApp.ShoppingList.Service;
 
+import com.portfolio.TellMeWhenApp.Mapper.ShoppingProductMapper;
+import com.portfolio.TellMeWhenApp.ShoppingList.Dto.ShoppingProductDto;
+import com.portfolio.TellMeWhenApp.ShoppingList.Model.ShoppingProduct;
+import com.portfolio.TellMeWhenApp.ShoppingList.Repository.ShoppingProductRepository;
+import com.portfolio.TellMeWhenApp.StorageProduct.Service.GenericProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor
 @Service
-public class ShoppingProductServiceImpl {
+public class ShoppingProductServiceImpl implements GenericProductService<ShoppingProductDto> {
 
+    ShoppingProductRepository productRepository;
+    ShoppingProductMapper productMapper;
 
+    @Override
+    public void save(ShoppingProductDto shoppingProductDto) {
+        productRepository.save(productMapper.mapDtoIntoEntity(shoppingProductDto));
+    }
+
+    public void update(ShoppingProductDto shoppingProductDto) {
+    }
+
+    @Override
+    public void delete(Integer id) {productRepository.deleteById(id);
+    }
+
+    @Override
+    public ShoppingProductDto findOne(Integer id) {
+        return productMapper.mapEntityIntoDto(productRepository.findById(id).orElseThrow());
+    }
+
+    @Override
+    public List<ShoppingProductDto> getAll() {
+        List<ShoppingProduct> shoppingProductEntities = productRepository.findAll();
+        List<ShoppingProductDto> shoppingProductDtoList = new ArrayList<>();
+        for (ShoppingProduct entity : shoppingProductEntities) {
+            shoppingProductDtoList.add(productMapper.mapEntityIntoDto(entity));
+        }
+        return shoppingProductDtoList;
+    }
 
 
 }
