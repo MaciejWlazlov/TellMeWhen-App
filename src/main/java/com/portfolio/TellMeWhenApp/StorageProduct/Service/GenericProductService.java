a1d8f11ba3aaf76500d5502684fcea0131f1f48e
MaciexDrop <macwla1994@gmail.com>
2022-05-04 09:32:41 +0200

    Changed service into generic service class

---
+++ src/main/java/com/portfolio/TellMeWhenApp/StorageProduct/Service/GenericProductService.java
@@ -0,0 +1,16 @@
+package com.portfolio.TellMeWhenApp.StorageProduct.Service;
+
+import java.util.List;
+
+public interface GenericProductService<T> {
+
+    void save(T entity);
+
+    void update(T entity);
+
+    void delete(Integer id);
+
+    T findOne(Integer id);
+
+    List<T> getAll();
+}
