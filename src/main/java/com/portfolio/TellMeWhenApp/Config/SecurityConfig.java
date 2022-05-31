e318e867e456178a00783d164baa74951c3dd5cf
MaciexDrop <macwla1994@gmail.com>
2022-05-31 12:59:12 +0200

    Added Security configuration

---
+++ src/main/java/com/portfolio/TellMeWhenApp/Config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.portfolio.TellMeWhenApp.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.headers().disable();
+        http
+                .authorizeRequests().anyRequest().authenticated();
+    }
+
+}
