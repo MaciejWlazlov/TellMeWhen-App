65c554c717f7cee722be3030d511def9d0493c03
MaciexDrop <macwla1994@gmail.com>
2022-05-31 12:21:55 +0200

    Added UserDetailsService implementation

---
+++ src/main/java/com/portfolio/TellMeWhenApp/User/Service/MyUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.portfolio.TellMeWhenApp.User.Service;
+
+import com.portfolio.TellMeWhenApp.User.Repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).get();
+    }
+}
